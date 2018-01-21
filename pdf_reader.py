ce1263e9b244c7e34216c39721527eb8f553c8a6
abdujaparov <afmulone@gmail.com>
2018-01-21 14:58:58 +0100

    Read page

--- pdf_reader.py
+++ pdf_reader.py
@@ -2,6 +2,12 @@ import PyPDF2
 import pathlib
 
 
+def __getFornitura(fp):
+    fornitura=''
+
+    return fornitura
+
+
 filename = 'C:\projectPython\data\eletrica\iren\Fattura Iren 1515244_es.pdf'
 
 if pathlib.Path(filename).is_file() == False:    
@@ -10,11 +16,19 @@ if pathlib.Path(filename).is_file() == False:
 pdf_read = open (filename,'rb')
 pdf_read_start = PyPDF2.PdfFileReader(pdf_read)
 
-print('Numero di pagine',pdf_read_start.getNumPages())
-print('Informazioni Documento',pdf_read_start.getDocumentInfo())
+page0=''
+
+if(pdf_read_start.getNumPages() > 0):
+    pageObj = pdf_read_start.getPage(0)
+    page0=pageObj.extractText()
+
+print('Lunghezza stringa pagina:\n',len(page0))
+print('Pagina')
+print(page0)
+__getFornitura(page0)
 
 
-
+pdf_read.close()
 
 
 

690c76f472f3cbd0d95598ab1e445cc0e4f7454b
angelo <angelo@angelo-to-ubuntu>
2018-02-24 12:03:39 +0100

    Creata la classe per scrivere su elasticsearch

---
+++ dataStore/ElasticSearchStore.py
@@ -0,0 +1,17 @@
+'''
+Created on 18 feb 2018
+
+@author: angelo
+'''
+
+class ElasticSearchStore(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
